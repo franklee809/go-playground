1fefa65e9472a9a697a37cd6546c24bde8db3a7a
Lee <franklee809@gmail.com>
2025-11-02 17:46:31 +0800

    feat: go variable and types

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module myapp
+module investment_calculator
 
 go 1.25.3
---
+++ src/investment_calculator.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"math"
+)
+
+func main() {
+	var investmentAmount int = 100
+	var expectedReturnRate float64 = 5.5
+
+	var years int = 10
+
+	var futureValue float64 = float64(investmentAmount) * math.Pow(1+expectedReturnRate/100, float64(years))
+
+	println("futureValue : ", futureValue)
+
+}
