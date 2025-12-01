838ae937602016ccff6109ecfc17da96942f37b2
frank <franklee809@gmail.com>
2025-12-01 16:21:45 +0800

    feat: enhance profit calculator with error handling and file writing

--- cmd/calculator/profit_calculator.go
+++ cmd/calculator/profit_calculator.go
@@ -1,18 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"os"
+)
 
 func main() {
 	// Revenue
-	revenue := handleUserInput("Enter revenue: ")
-	expenses := handleUserInput("Enter Expense: ")
-	tax_rate := handleUserInput("Enter Tax rate: ")
+	revenue, err := handleUserInput("Enter revenue: ")
+
+	if err != nil {
+		panic(err)
+	}
+	expenses, err := handleUserInput("Enter Expense: ")
+	if err != nil {
+		panic(err)
+	}
+	tax_rate, err := handleUserInput("Enter Tax rate: ")
+
+	if err != nil {
+		panic(err)
+	}
 
 	ebt_value, earning_after_tax, ratio := calculateFinancials(revenue, expenses, tax_rate)
 	fmt.Printf("EBT : %.1f \n", ebt_value)
 	fmt.Printf("Profit : %.1f \n", earning_after_tax)
 	fmt.Printf("Ratio : %.12f \n", ratio)
-
+	writeBalanceToFile("ebt_value", ebt_value)
+	writeBalanceToFile("earning_after_tax", earning_after_tax)
+	writeBalanceToFile("ratio", ratio)
 }
 func calculateFinancials(revenue, expenses, taxRate float64) (float64, float64, float64) {
 	ebt_value := revenue - expenses
@@ -24,12 +41,19 @@ func calculateFinancials(revenue, expenses, taxRate float64) (float64, float64, 
 	return ebt_value, earning_after_tax, ratio
 }
 
-func handleUserInput(infoText string) float64 {
+func handleUserInput(infoText string) (float64, error) {
 	input := 0.0
 	// Revenue
 	print(infoText)
 
 	fmt.Scan(&input)
+	if input <= 0 {
+		return 0, errors.New("value must be positive")
+	}
+	return input, nil
+}
 
-	return input
+func writeBalanceToFile(name string, amount float64) {
+	var amountString string = fmt.Sprint(amount)
+	os.WriteFile(name, []byte(amountString), os.FileMode(0644))
 }
