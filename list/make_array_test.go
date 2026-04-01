3c734b65a6a0b333979eaa20ceb59e480409e6f2
frank <franklee809@gmail.com>
2026-04-01 13:59:29 +0800

    feat: implement floatMap type and output method for enhanced array management

--- list/make_array_test.go
+++ list/make_array_test.go
@@ -5,6 +5,13 @@ import (
 	"testing"
 )
 
+// type aliases
+type floatMap map[string]float64
+
+func (m floatMap) output() {
+	fmt.Println(m)
+}
+
 func TestMakeArray(t *testing.T) {
 	// for memory managements
 	userNames := make([]string, 2, 5)
@@ -17,11 +24,23 @@ func TestMakeArray(t *testing.T) {
 	userNames = append(userNames, "Manuel")
 	userNames = append(userNames, "Manuel")
 	userNames = append(userNames, "Manuel")
-	fmt.Println(userNames)
+	// fmt.Println(userNames)
 
-	courseRatings := make(map[string]float64)
+	courseRatings := make(floatMap, 3)
 
 	courseRatings["go"] = 4.7
 	courseRatings["react"] = 5.8
-	fmt.Println(courseRatings)
+	courseRatings["angular"] = 5.12
+	// fmt.Println(courseRatings)
+	courseRatings.output()
+
+	for index, value := range userNames {
+		fmt.Println("index: ", index)
+		fmt.Println("value: ", value)
+	}
+
+	for key, value := range courseRatings {
+		fmt.Println("key : ", key)
+		fmt.Println("value : ", value)
+	}
 }
