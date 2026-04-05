ffdd8dd0cd2c44df9ccb156b2a6ad9b1e3a4f443
frank <franklee809@gmail.com>
2026-04-05 21:13:21 +0800

    feat: refactor TestMakeArray to utilize getTransformerFunction and improve transformation logic

--- function/main_test.go
+++ function/main_test.go
@@ -7,17 +7,36 @@ import (
 
 type transformFn func(int) int
 
+type anotherFn func(int, []string, map[string][]int) ([]int, string)
+
+func getTransformerFunction(numbers *[]int) transformFn {
+	if (*numbers)[0] == 1 {
+		return double
+	}
+	return triple
+}
+
 func TestMakeArray(t *testing.T) {
 	numbers := []int{1, 23, 34, 5}
-	double := doubleNumbers(&numbers, double)
-	triple := doubleNumbers(&numbers, triple)
+	moreNumbers := []int{5, 1, 2}
+	double := transformNumbers(&numbers, double)
+	triple := transformNumbers(&numbers, triple)
 
 	fmt.Println("numbers :", numbers)
 	fmt.Println("double :", double)
 	fmt.Println("triple :", triple)
+
+	transformFn1 := getTransformerFunction(&numbers)
+	transformFn2 := getTransformerFunction(&moreNumbers)
+
+	transformedNumbers := transformNumbers(&numbers, transformFn1)
+	moreTransformedNumbers := transformNumbers(&moreNumbers, transformFn2)
+
+	fmt.Println(transformedNumbers)
+	fmt.Println(moreTransformedNumbers)
 }
 
-func doubleNumbers(numbers *[]int, transform transformFn) []int {
+func transformNumbers(numbers *[]int, transform transformFn) []int {
 	dNumbers := []int{}
 	for _, value := range *numbers {
 		fmt.Println(value)
