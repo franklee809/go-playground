b8a298aeb3facbce3f17de0f02b4d5977fd78e14
frank <franklee809@gmail.com>
2026-04-08 14:23:47 +0800

    feat(tutorial): function factory pattern with closure

--- function/anony_test.go
+++ function/anony_test.go
@@ -8,9 +8,20 @@ import (
 func TestAnonyFunction(t *testing.T) {
 	numbers := []int{1, 2, 3}
 
-	fmt.Println("numbers :", numbers)
+	double := createTransfomer(2)
+	triple := createTransfomer(3)
 
 	transformed := transformNumbers(&numbers, func(number int) int { return number * 2 })
 
+	doubled := transformNumbers(&numbers, double)
+	tripled := transformNumbers(&numbers, triple)
 	fmt.Println(transformed)
+	fmt.Println(doubled)
+	fmt.Println(tripled)
+}
+
+func createTransfomer(factor int) func(int) int {
+	return func(number int) int {
+		return number * factor
+	}
 }
